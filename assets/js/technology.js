55b99f2f526260eeeebf57d105d92eba42b1ef72
JAEYOUNG PARK <jyhome1228@gmail.com>
2026-07-27 09:57:18 +0900

    Refine technology page role layout

--- assets/js/technology.js
+++ assets/js/technology.js
@@ -3,6 +3,8 @@ const technologyToggle=document.querySelector("[data-technology-toggle]");
 const technologyState=document.getElementById("technology-state");
 const technologyHero=document.querySelector("[data-technology-hero]");
 
+document.querySelectorAll(".role-grid article").forEach(article=>article.style.position="relative");
+
 if(technologyGrid&&technologyToggle){
   const nodes=[...technologyGrid.querySelectorAll(".tech-node")];
   nodes.forEach(node=>node.style.setProperty("--order",node.dataset.order||1));
